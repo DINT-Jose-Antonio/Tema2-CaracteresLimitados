09bc61efb4f13c2824e5f297140a0df4ffa66f41
Jose Antonio Guerrero <ja.guerrero@iesdoctorbalmis.es>
2022-09-30 00:21:39 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CuadroTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ContadorTextBlock.Text = CuadroTextBox.Text.Length.ToString() + "/140";
+            if (ContadorTextBlock.Text == "140/140")
+            {
+                CuadroTextBox.IsReadOnly = true;
+            }
+        }
+    }
+}
